3cc4c137ec378c95c51462ef8c92024cfcb5f911
Marius L <mariusleica@gmail.com>
2022-07-07 16:56:22 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+$paragrafo = "Lorem ipsum dolor sit amet consectetur adipisicing elit. In blanditiis accusantium asperiores atque odio tempora, esse nisi saepe quisquam repellat voluptatibus modi provident iusto animi laborum rerum? Molestiae, nulla tenetur?";
+
+?>
+<h1>funziona</h1>
+<p>
+    <?php echo $paragrafo; ?>
+</p>
